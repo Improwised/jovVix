8eef8eeffc9c4c485f708593e01341a8685b20b3
Jay-Ponda-Improwised <jay.ponda@improwised.com>
2024-02-11 03:06:29 +0530

    reformat hooks files and pre-commit

--- api/hooks/go-vet.sh
+++ api/hooks/go-vet.sh
@@ -1,3 +1,19 @@
 #!/bin/bash
 
-go vet ./api/...
+set -eu -o pipefail
+
+# Check if a go.work file exists in the current directory
+if [ -f "go.work" ]; then
+    # Iterate through every folder in the current directory
+    for dir in */; do
+        dir=${dir%/}
+        # Check if a go.mod file exists in the folder
+        if ls "$dir"/*.go &> /dev/null; then
+            echo "Go module in folder: $(pwd)/$dir"
+            # Run lint on that folder
+            go vet ./$dir/... "$@"
+        fi
+    done
+else
+    echo "No go.work file found in the current directory"
+fi
