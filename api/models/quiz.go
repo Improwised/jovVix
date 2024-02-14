fb31005ef8742db0c7793948fa88f7ed5cb19057
Jay-Ponda-Improwised <jay.ponda@improwised.com>
2024-02-15 04:34:38 +0530

    write models and migrations

--- api/models/quiz.go
+++ api/models/quiz.go
@@ -1,13 +1,30 @@
 package models
 
-import "github.com/doug-martin/goqu/v9"
+import (
+	"time"
 
+	"github.com/doug-martin/goqu/v9"
+	"github.com/google/uuid"
+)
+
+// Quiz model
 type Quiz struct {
+	ID          uuid.UUID `json:"id" db:"id"`
+	Title       string    `json:"title" db:"title" validate:"required"`
+	Description string    `json:"description,omitempty" db:"description"`
+	CreatorID   string    `json:"creator_id,omitempty" db:"creator_id"`
+	CreatedAt   time.Time `json:"created_at,omitempty" db:"created_at,omitempty"`
+	UpdatedAt   time.Time `json:"updated_at,omitempty" db:"updated_at,omitempty"`
+}
+
+// QuizModel implements quiz related database operations
+type QuizModel struct {
 	db *goqu.Database
 }
 
-func NewQuiz(db *goqu.Database) *Quiz{
-	return &Quiz{db}
+// InitQuizModel initializes the QuizModel
+func InitQuizModel(goquDB *goqu.Database) *QuizModel {
+	return &QuizModel{db: goquDB}
 }
 
 func (q *Quiz) GetQuestions() {
