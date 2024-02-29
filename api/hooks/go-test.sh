46d8ad263d556c0e35621e100aff4ef9b2400e3b
Jay-Ponda-Improwised <jay.ponda@improwised.com>
2024-02-29 15:49:57 +0530

    remove go.work file

--- api/hooks/go-test.sh
+++ api/hooks/go-test.sh
@@ -8,19 +8,9 @@ if ! command -v gotestsum &> /dev/null ; then
     exit 1
 fi
 
-# Check if a go.work file exists in the current directory
-if [ -f "go.work" ]; then
-    # Iterate through every folder in the current directory
-    for dir in */; do
-        dir=${dir%/}
-        # Check if a go.mod file exists in the folder
-        if ls "$dir"/*.go &> /dev/null; then
-            echo "Testing module in folder: $(pwd)/$dir"
-            # Run lint on that folder
-            source ./$dir/.env.testing && gotestsum --format pkgname -- -coverprofile=cover.out ./$dir/... "$@"
-        fi
-    done
+# Check if a api folder exists in the current directory
+if [ -d "api" ]; then
+    cd api && source .env.testing && gotestsum --format pkgname -- -coverprofile=cover.out ./... "$@"
 else
     source .env.testing && gotestsum --format pkgname -- -coverprofile=cover.out ./... "$@"
-    echo "No go.work file found in the current directory"
 fi
