d8146a99ac89fd61cb366102cf703d8cd435487d
Ridham Parmar <ridham.parmar@improwised.com>
2024-04-03 14:32:53 +0530

    feat: added layout for final scoreboard

--- app/pages/join/scoreboard.vue
+++ app/pages/join/scoreboard.vue
@@ -1,1 +1,21 @@
-<template><div>this is final score board</div></template>
+<template>
+  <div class="table-responsive mt-5">
+    <table
+      class="table table-striped table-hover table-borderless table-light align-middle"
+    >
+      <thead class="table-light">
+        <caption>
+          Rankings
+        </caption>
+        <tr>
+          <th>Rank</th>
+          <th>User</th>
+          <th>Score</th>
+          <th>Response Time</th>
+        </tr>
+      </thead>
+      <tbody class="table-group-divider"></tbody>
+      <tfoot></tfoot>
+    </table>
+  </div>
+</template>
