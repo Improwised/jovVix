09dc2bf4967a27e1576181653f59a3ba3343e1e5
husen kureshi <husen.kureshi@improwised.com>
2024-08-23 11:31:33 +0530

    Revert "adding the sort by latest and paginations to reports page (#311)"
    This reverts commit 5aac6dcdc5d917ce06fd9e2e0f8c339df40186a3.

--- app/pages/admin/reports/index.vue
+++ app/pages/admin/reports/index.vue
@@ -8,7 +8,7 @@
         <h4 class="mb-0">Quiz Reports</h4>
       </div>
       <div class="table-responsive">
-        <<table class="table text-nowrap mb-0 table-hover">
+        <table class="table text-nowrap mb-0">
           <thead class="table-light">
             <tr>
               <th>Quiz Name</th>
@@ -20,12 +20,8 @@
             </tr>
           </thead>
           <tbody>
-            <tr
-              v-for="(quiz, index) in paginatedQuizList"
-              :key="index"
-              class="clickable-row"
-              @click="navigateToQuiz(quiz.id)"
-            >
+            <div v-if="!quizList?.length">No quiz found....</div>
+            <tr v-for="(quiz, index) in quizList" :key="index">
               <td>
                 <div class="ms-3 lh-1">
                   <h5 class="mb-1">
@@ -53,32 +49,11 @@
           </tbody>
         </table>
       </div>
-      <!-- Pagination Controls -->
-      <div class="mt-3">
-        <button
-          :disabled="currentPage === 1"
-          @click="currentPage--"
-          class="btn btn-primary me-2"
-        >
-          Previous
-        </button>
-        <button
-          :disabled="currentPage >= totalPages"
-          @click="currentPage++"
-          class="btn btn-primary"
-        >
-          Next
-        </button>
-      </div>
     </div>
   </div>
 </template>
-
 <script setup>
-import { ref, computed } from "vue";
 import { format, formatISO } from "date-fns";
-import { useRouter } from 'vue-router';
-
 
 definePageMeta({
   layout: "default",
@@ -86,64 +61,25 @@ definePageMeta({
 
 const { api_url } = useRuntimeConfig().public;
 const headers = useRequestHeaders(["cookie"]);
-const router = useRouter();
-
-
-const itemsPerPage = 8;
-const currentPage = ref(1);
-
-
 
 const {
   data: quizList,
   error: quizListError,
   pending: quizListPending,
 } = useFetch(`${api_url}/admin/reports/list`, {
-  transform: (quizList) => {
-    return quizList.data
-      .map((quiz) => {
-        quiz.activated_from.Time = format(
-          formatISO(quiz.activated_from.Time),
-          "dd-MMM-yyyy HH:mm:ss"
-        );
-        quiz.activated_to.Time = format(
-          formatISO(quiz.activated_to.Time),
-          "dd-MMM-yyyy HH:mm:ss"
-        );
-        return quiz;
-      })
-      .sort((a, b) => new Date(b.activated_from.Time) - new Date(a.activated_from.Time)); // Sorting by activated_from.Time in descending order
-  },
+  transform: (quizList) =>
+    quizList.data?.map((quiz) => {
+      quiz.activated_from.Time = format(
+        formatISO(quiz.activated_from.Time),
+        "dd-MMM-yyyy HH:mm:ss"
+      );
+      quiz.activated_to.Time = format(
+        formatISO(quiz.activated_to.Time),
+        "dd-MMM-yyyy HH:mm:ss"
+      );
+      return quiz;
+    }),
   credentials: "include",
   headers: headers,
 });
-
-// Computed property to get the paginated list
-const paginatedQuizList = computed(() => {
-  const start = (currentPage.value - 1) * itemsPerPage;
-  const end = start + itemsPerPage;
-  return quizList.value.slice(start, end);
-});
-
-// Computed property to get the total number of pages
-const totalPages = computed(() => {
-  return Math.ceil(quizList.value.length / itemsPerPage);
-});
-
-// Function to navigate to the quiz details page
-const navigateToQuiz = (quizId) => {
-  router.push(`/admin/reports/${quizId}`);
-}
 </script>
-
-<style scoped>
-/* Applying light purple border on hover for table rows */
-.table-hover tbody tr {
-  transition: border 0.2s ease-in-out;
-}
-
-.table-hover tbody tr:hover {
-  border: 2px solid #3d3d7e; /* Light purple border */
-  cursor: pointer;
-}
-</style>
