9c5184adb839e10cc12f1cd5333c55363b096711
husen.kureshi <husen.kureshi@improwised.com>
2024-10-25 13:49:54 +0000

    fix responsive issue of scoreboard table (#403)
    Reviewed-on: https://git.pride.improwised.dev/Improwised/quizz-app/pulls/403

--- app/components/ScoreBoardTable.vue
+++ app/components/ScoreBoardTable.vue
@@ -1,54 +1,60 @@
 <template>
-  <v-card elevation="0" class="table-responsive container pb-5">
+  <v-card elevation="0" class="container pb-5">
     <v-card-item>
       <div class="d-flex align-center justify-center">
         <div>
           <h5 class="text-h5 mb-1 font-weight-semibold">Rankings</h5>
         </div>
       </div>
-      <table class="table align-middle table-bordered">
-        <thead>
-          <tr>
-            <th>Rank</th>
-            <th>User</th>
-            <th>Score</th>
-          </tr>
-        </thead>
-        <tbody class="table-group-divider">
-          <tr
-            v-for="(user, index) in scoreboardData"
-            :key="index"
-            :class="{
-              'table-primary':
-                user.username === props.userName && !props.isAdmin,
-            }"
-          >
-            <td>
-              {{ user.rank }}
-            </td>
-            <td v-if="props.isAdmin">
-              <img
-                :src="`${getAvatarUrlByName(user?.img_key)}&scale=75`"
-                alt="Avatar"
-                height="50px"
-              />
-              {{ user.firstname }} <span>({{ user.username }})</span>
-            </td>
-            <td v-else>
-              <img
-                :src="`${getAvatarUrlByName(user?.img_key)}&scale=75`"
-                alt="Avatar"
-                height="50px"
-              />
-              {{ user.firstname }}
-              <span v-if="props?.userName === user.username">
-                &nbsp;({{ user.username }})
-              </span>
-            </td>
-            <td>{{ user.score }}</td>
-          </tr>
-        </tbody>
-      </table>
+      <div class="table-responsive">
+        <table class="table align-middle table-bordered">
+          <thead>
+            <tr>
+              <th>Rank</th>
+              <th>User</th>
+              <th>Score</th>
+            </tr>
+          </thead>
+          <tbody class="table-group-divider">
+            <tr
+              v-for="(user, index) in scoreboardData"
+              :key="index"
+              :class="{
+                'table-primary':
+                  user.username === props.userName && !props.isAdmin,
+              }"
+            >
+              <td>
+                {{ user.rank }}
+              </td>
+              <td v-if="props.isAdmin">
+                <div class="text-nowrap">
+                  <img
+                    :src="`${getAvatarUrlByName(user?.img_key)}&scale=75`"
+                    alt="Avatar"
+                    height="50px"
+                  />
+                  <span>{{ user.firstname }} ({{ user.username }})</span>
+                </div>
+              </td>
+              <td v-else>
+                <div class="text-nowrap">
+                  <img
+                    :src="`${getAvatarUrlByName(user?.img_key)}&scale=75`"
+                    alt="Avatar"
+                    height="50px"
+                  />
+                  <span>{{ user.firstname }}</span>
+                  <span v-if="props?.userName === user.username">
+                    &nbsp;({{ user.username }})
+                  </span>
+                </div>
+              </td>
+              <td>{{ user.score }}</td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
     </v-card-item>
   </v-card>
 </template>
