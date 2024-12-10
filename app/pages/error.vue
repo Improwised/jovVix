6db1ae2cc0d6a12bd993d8a56b01f1298120daff
husen kureshi <husen.kureshi@improwised.com>
2024-12-10 07:01:47 +0000

    feat: change ui port to 5000 and add kratos env file (issue #430) (#434)
    Reviewed-on: https://git.pride.improwised.dev/Improwised/quizz-app/pulls/434
    Reviewed-by: khushijoshi <khushi.joshi@improwised.com>
    Co-authored-by: husen kureshi <husen.kureshi@improwised.com>
    Co-committed-by: husen kureshi <husen.kureshi@improwised.com>

--- app/pages/error.vue
+++ app/pages/error.vue
@@ -1,10 +1,12 @@
 <script setup>
 const route = useRoute();
 
-const status = ref(route.query.status);
+const status = ref(
+  route.query.status || "Something went wrong. Please try again later."
+);
 const message = ref(route.query.error);
 </script>
 
 <template>
-  <Frame :page-title="status.toUpperCase()" :page-message="message"></Frame>
+  <Frame :page-title="status?.toUpperCase()" :page-message="message"></Frame>
 </template>
