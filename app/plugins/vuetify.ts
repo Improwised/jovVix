e3100cd815491903ebcdddaffae79f03c76efc4a
husen kureshi <husen.kureshi@improwised.com>
2024-12-12 06:14:58 +0000

    fix: import bootstrap style in vuetify plugins to prevent overriding the bootstrap style by vuetify. (#435)
    ### issue and solution:
    - for now the primary and other colors are overwrite by vuetify. For fix this issue import the bootstrap file in vuetify plugin.
    
    Reviewed-on: https://git.pride.improwised.dev/Improwised/quizz-app/pulls/435
    Reviewed-by: khushijoshi <khushi.joshi@improwised.com>
    Co-authored-by: husen kureshi <husen.kureshi@improwised.com>
    Co-committed-by: husen kureshi <husen.kureshi@improwised.com>

--- app/plugins/vuetify.ts
+++ app/plugins/vuetify.ts
@@ -1,8 +1,8 @@
 // import this after install `@mdi/font` package
 import "@mdi/font/css/materialdesignicons.css";
-
 import "vuetify/styles";
 import { createVuetify } from "vuetify";
+import "~/assets/scss/theme.scss"
 
 export default defineNuxtPlugin((app) => {
   const vuetify = createVuetify({
