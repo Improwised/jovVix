8b45ea8853c788f335ead5cafc0d4f989a62e519
Ashvin Bambhaniya <ashvin.bambhaniya@improwised.com>
2025-08-01 11:35:12 +0530

    feat: refactor Vuetify plugin to import only necessary components and styles for improved performance

--- app/plugins/vuetify.ts
+++ app/plugins/vuetify.ts
@@ -1,12 +1,47 @@
-// import this after install `@mdi/font` package
-import "@mdi/font/css/materialdesignicons.css";
-import "vuetify/styles";
+// Import only specific Vuetify components and styles used in the app
 import { createVuetify } from "vuetify";
+import {
+  VBtn,
+  VCard,
+  VCardText,
+  VCardTitle,
+  VFileInput,
+  VListItemTitle,
+  VOtpInput,
+  VProgressCircular,
+  VProgressLinear,
+  VSkeletonLoader,
+} from "vuetify/components";
+import { aliases, mdi } from "vuetify/iconsets/mdi-svg";
+// Import only essential Vuetify styles - use the correct paths
+import "vuetify/styles";
 import "~/assets/scss/theme.scss";
 
 export default defineNuxtPlugin((app) => {
   const vuetify = createVuetify({
     ssr: true,
+    components: {
+      VBtn,
+      VCard,
+      VCardText,
+      VCardTitle,
+      VFileInput,
+      VSkeletonLoader,
+      VProgressCircular,
+      VProgressLinear,
+      VListItemTitle,
+      VOtpInput,
+    },
+    icons: {
+      defaultSet: "mdi",
+      aliases,
+      sets: {
+        mdi,
+      },
+    },
+    theme: {
+      defaultTheme: "light",
+    },
   });
   app.vueApp.use(vuetify);
 });
