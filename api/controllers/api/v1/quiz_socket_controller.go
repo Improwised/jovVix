664946fb921538a4e4f2a888c77d9e01a0a889fa
Smit Dodiya <smit.dodiya@improwised.com>
2026-03-23 14:42:37 +0530

    fix: deactivate session on terminate (#155)
    * fix: deactivate session on terminate and lock edits only for active runs
    
    * fix: revert logic of edit access
    
    * fix: remove redundant UUID parsing before deactivate
    
    * fix: add logging to Terminate endpoint error handling

--- api/controllers/api/v1/quiz_socket_controller.go
+++ api/controllers/api/v1/quiz_socket_controller.go
@@ -745,14 +745,14 @@ func handleConnectedUser(c *websocket.Conn, qc *quizSocketController, sessionId 
 			err = func() error {
 				arrangeMu.Lock()
 				defer arrangeMu.Unlock()
-				return utils.JSONSuccessWs( c, constants.EventSendInvitationCode, response )
+				return utils.JSONSuccessWs(c, constants.EventSendInvitationCode, response)
 			}()
 			if err != nil {
 				qc.logger.Error("error while sending initial user data to admin", zap.Error(err))
 			}
 		}
 	}
-	
+
 	pubsub := qc.redis.PubSubModel.Client.Subscribe(qc.redis.PubSubModel.Ctx, fmt.Sprintf("%s-%s", constants.ChannelUserJoin, sessionId), fmt.Sprintf("%s-%s", constants.ChannelUserDisconnect, sessionId), constants.EventTerminateQuiz, constants.EventStartQuizByAdmin, constants.StartQuizByAdminNoPlayerFound)
 	defer func() {
 		if pubsub != nil {
@@ -993,7 +993,7 @@ func sendSingleQuestion(c *websocket.Conn, qc *quizSocketController, wg *sync.Wa
 	}
 
 	var questionStartTime time.Time
-	
+
 	// start counter if not any question running
 	if !lastQuestionTimeStamp.Valid {
 		response.Component = constants.Question
@@ -1023,7 +1023,7 @@ func sendSingleQuestion(c *websocket.Conn, qc *quizSocketController, wg *sync.Wa
 		"quiz_id":        question.QuizId,
 		"no":             question.OrderNumber,
 		"duration":       question.DurationInSeconds,
-		"start_time":     questionStartTime.Format(time.RFC3339), 
+		"start_time":     questionStartTime.Format(time.RFC3339),
 		"question":       question.Question,
 		"options":        question.Options,
 		"question_media": question.QuestionMedia,
@@ -1355,5 +1355,39 @@ func (qc *quizSocketController) SetAnswer(c *fiber.Ctx) error {
 }
 
 func (ctrl *quizSocketController) Terminate(c *fiber.Ctx) error {
+	sessionId := c.Query(constants.SessionIDParam)
+	if sessionId == "" {
+		return utils.JSONFail(c, http.StatusBadRequest, "session_id is required")
+	}
+
+	userId := quizUtilsHelper.GetString(c.Locals(constants.ContextUid))
+	if userId == "" {
+		return utils.JSONError(c, http.StatusUnauthorized, constants.ErrUnauthenticated)
+	}
+
+	session, err := ctrl.activeQuizModel.GetSession(sessionId)
+	if err != nil {
+		if err == sql.ErrNoRows {
+			ctrl.logger.Error("session not found during terminate", zap.Error(err))
+			return utils.JSONFail(c, http.StatusBadRequest, constants.ErrSessionNotFound)
+		}
+		ctrl.logger.Error("error getting session during terminate", zap.Error(err))
+		return utils.JSONError(c, http.StatusInternalServerError, constants.UnknownError)
+	}
+
+	if session.AdminID != userId {
+		return utils.JSONError(c, http.StatusUnauthorized, constants.ErrUnauthorized)
+	}
+
+	err = ctrl.activeQuizModel.Deactivate(session.ID)
+	if err != nil {
+		if err == sql.ErrNoRows {
+			ctrl.logger.Error("session not found during deactivate", zap.Error(err))
+			return utils.JSONFail(c, http.StatusBadRequest, constants.ErrSessionNotFound)
+		}
+		ctrl.logger.Error("error deactivating session", zap.Error(err))
+		return utils.JSONError(c, http.StatusInternalServerError, constants.UnknownError)
+	}
+
 	return utils.JSONSuccess(c, http.StatusOK, nil)
 }
