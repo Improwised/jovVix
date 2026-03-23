dfd32f4e825340895a98e519d8c5ab7942af733b
Smit Dodiya <smit.dodiya@improwised.com>
2026-03-23 14:42:53 +0530

    fix: add and fix test cases for utils and quiz helper functions (#168)

--- api/helpers/utils/validating_test.go
+++ api/helpers/utils/validating_test.go
@@ -1,20 +1,37 @@
 package quizUtilsHelper
 
 import (
+	"strconv"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
+	"github.com/Improwised/jovvix/api/constants"
 )
 
-func TestValidatingCode(t *testing.T) {
-	t.Run("Test validate code", func(t *testing.T) {
-		expected := true
-		actual := IsValidCode("123456")
-		assert.Equal(t, expected, actual)
+func TestIsValidCode(t *testing.T) {
 
-		expected = false
-		actual = IsValidCode("9999999")
-		assert.Equal(t, expected, actual)
+	t.Run("valid code", func(t *testing.T) {
+		validCode := strconv.Itoa(constants.MinInvitationCode)
+		assert.True(t, IsValidCode(validCode))
+	})
 
+	t.Run("invalid length", func(t *testing.T) {
+		assert.False(t, IsValidCode("12345"))
+		assert.False(t, IsValidCode("1234567"))
+	})
+
+	t.Run("non numeric code", func(t *testing.T) {
+		assert.False(t, IsValidCode("12a456"))
+		assert.False(t, IsValidCode("abcdef"))
+	})
+
+	t.Run("out of range", func(t *testing.T) {
+		// below min
+		belowMin := strconv.Itoa(constants.MinInvitationCode - 1)
+		assert.False(t, IsValidCode(belowMin))
+
+		// above max
+		aboveMax := strconv.Itoa(constants.MaxInvitationCode + 1)
+		assert.False(t, IsValidCode(aboveMax))
 	})
 }
