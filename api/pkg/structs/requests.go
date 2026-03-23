9d8f919299709f9db722dd2047dc6c31cd97a3d4
smitdodiya <smit.dodiya@improwised.com>
2026-03-23 17:54:13 +0530

    fix: make email immutable

--- api/pkg/structs/requests.go
+++ api/pkg/structs/requests.go
@@ -18,7 +18,6 @@ type ReqRegisterUser struct {
 type ReqUpdateUser struct {
 	FirstName string `json:"first_name" validate:"required"`
 	LastName  string `json:"last_name" validate:"required"`
-	Email     string `json:"email" validate:"required,email"`
 }
 
 type ReqLoginUser struct {
